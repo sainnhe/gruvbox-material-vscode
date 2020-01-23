ecf1d6c81b5d45cbdaa8f3fa88d23eeeebfeaa65
Sainnhepark <sainnhe@gmail.com>
2020-01-23 16:50:43 +0800

    basically works

--- scripts/generate.ts
+++ scripts/generate.ts
@@ -1,19 +1,12 @@
 import { join } from "path";
-import * as fs from "fs";
 import { default as data } from "../src/utils/themeData";
-
-export async function writeFile(path: string, data: unknown) {
-  return new Promise((resolve, reject) => {
-    fs.writeFile(path, JSON.stringify(data, null, 2), err =>
-      err ? reject(err) : resolve()
-    );
-  });
-}
+import { writeFile } from "../src/utils/writeFile";
 
 writeFile(
   join(__dirname, "..", "themes", "gruvbox-material-dark.json"),
   data.dark
 );
+
 writeFile(
   join(__dirname, "..", "themes", "gruvbox-material-light.json"),
   data.light
--- src/extension.ts
+++ src/extension.ts
@@ -1,10 +1,23 @@
-import { workspace, commands } from "vscode";
+import { workspace, commands, ExtensionContext } from "vscode";
+import { join } from "path";
+import { default as data } from "./utils/themeData";
+import { writeFile } from "./utils/writeFile";
 
-export function activate(context: vscode.ExtensionContext) {
-  const configuration = workspace.getConfiguration('gruvboxMaterial');
-  let generateTheme = commands.registerCommand('gruvboxMaterial.generateTheme', () => {
-    
-  })
+export function activate(context: ExtensionContext) {
+  let generateTheme = commands.registerCommand(
+    "gruvboxMaterial.generateTheme",
+    () => {
+      writeFile(
+        join(__dirname, "..", "themes", "gruvbox-material-dark.json"),
+        data.dark
+      );
+      writeFile(
+        join(__dirname, "..", "themes", "gruvbox-material-light.json"),
+        data.light
+      );
+    }
+  );
+  context.subscriptions.push(generateTheme);
 }
 
 export function deactivate() {}
