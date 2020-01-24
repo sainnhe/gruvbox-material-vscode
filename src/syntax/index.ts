23a56428a14aef2a6e4a51bc9e361b1a57124396
Sainnhepark <sainnhe@gmail.com>
2020-01-24 21:53:31 +0800

    finish extended syntax

--- src/syntax/index.ts
+++ src/syntax/index.ts
@@ -1,9 +1,14 @@
 import { Configuration } from "../interface";
 import { getPalette } from "../palette";
 import { getCommonSyntax } from "./common";
+import { getExtendedSyntax } from "./extended";
 
 export function getSyntax(configuration: Configuration, variant: string) {
   let palette = getPalette(configuration, variant);
-  let syntax = getCommonSyntax(palette);
+  let commonSyntax = getCommonSyntax(palette);
+  let syntax = getExtendedSyntax(palette);
+  commonSyntax.forEach(item => {
+    syntax.push(item);
+  });
   return syntax;
 }
