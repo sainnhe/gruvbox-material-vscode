30b8d65e7a0579dcb224f574c8e6e1d4861cd494
Sainnhepark <sainnhe@gmail.com>
2020-01-24 10:59:42 +0800

    regenerate theme automatically when user configuration changes

--- src/extension.ts
+++ src/extension.ts
@@ -1,20 +1,20 @@
-import { commands, ExtensionContext } from "vscode";
+import { workspace } from "vscode";
 import { join } from "path";
 import { default as Utils } from "./utils";
 
-export function activate(context: ExtensionContext) {
-  let generateTheme = commands.registerCommand(
-    "gruvboxMaterial.generateTheme",
-    () => {
-      let utils = new Utils();
+export function activate() {
+  let utils = new Utils();
+
+  // regenerate theme files when user configuration changes
+  workspace.onDidChangeConfiguration(event => {
+    utils.detectConfigChanges(event, () => {
       utils.generate(
         join(__dirname, "..", "themes", "gruvbox-material-dark.json"),
         join(__dirname, "..", "themes", "gruvbox-material-light.json"),
         utils.getThemeData(utils.getConfiguration())
       );
-    }
-  );
-  context.subscriptions.push(generateTheme);
+    });
+  });
 }
 
 export function deactivate() {}
